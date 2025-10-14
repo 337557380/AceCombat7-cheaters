8c4b53fd4ea000cb86e38ffd215444f6f9eafb33
Doraemon <126883033+ForeverAM@users.noreply.github.com>
2025-10-14 21:08:22 +0800

    Update cheaters_update.sh

--- .github/cheaters_update.sh
+++ .github/cheaters_update.sh
@@ -35,6 +35,8 @@ echo ${cheaters_Name[*]}
 echo '# AceCombat7 cheaters
 Okay, there was a small issue. Until I resolve it, I'll continue using this version
 
+行吧，出了点小问题，在我解决之前，继续使用这个版本
+
 
 | Name | Steam URL ID | Aliases | Note |
 | ------ | ------ | ------ | ------ |' > README.md
